0fbb37cb51624acfaaa080754db16bf63c978aa3
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-05 00:09:57 +0300

    Fluent V. and AutoMapper - Added ProductDtoValidator class for validating ProductDto objects using FluentValidation. - Created MapProfile class for mapping between Product and ProductDto using AutoMapper.

---
+++ RestfulApiExample.Service/Mapping/MapProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RestfulApiExample.Core.DTOs;
+using RestfulApiExample.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Service.Mapping
+{
+	public class MapProfile : Profile
+	{
+		public MapProfile()
+		{
+			CreateMap<Product, ProductDto>().ReverseMap();
+		}
+	}
+}
---
+++ RestfulApiExample.Service/Validations/ProductDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using RestfulApiExample.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Services.Validations
+{
+	public class ProductDtoValidator : AbstractValidator<ProductDto>
+	{
+		public ProductDtoValidator()
+		{
+			RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+			RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+			RuleFor(x => x.Description).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+		}
+
+
+	}
+}
