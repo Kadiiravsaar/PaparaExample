674d4023c851741b86643b734c5f7018ecfec135
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-04 23:45:48 +0300

    Implement Repository and Service Layers with UnitOfWork Pattern
    - Created AppDbContext class inheriting from DbContext.
    - Configured DbContextOptions in the constructor.
    - Added DbSet<Product> to manage product entities.
    - Defined IGenericRepository<T> interface with CRUD operations.
    - Implemented GenericRepository<T> class for generic data access.
    - Defined IService<T> interface for service layer abstraction.
    - Implemented Service<T> class to handle business logic and transactions.
    - Integrated UnitOfWork pattern to manage database transactions.

---
+++ RestfulApiExample.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Core.Repositories
+{
+	public interface IGenericRepository<T> where T : class
+	{
+
+		IQueryable<T> GetAll();
+		Task<T> GetByIdAsync(int id);
+		Task<T> AddAsync(T entity);
+		void Remove(T entity);
+		void Update(T entity);
+	}
+}
---
+++ RestfulApiExample.Service/Services/Service.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestfulApiExample.Core.Repositories;
+using RestfulApiExample.Core.Services;
+using RestfulApiExample.Core.UnitOfWorks;
+using RestfulApiExample.Service.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Service.Services
+{
+	public class Service<T> : IService<T> where T : class
+	{
+		private readonly IGenericRepository<T> _repository;
+		private readonly IUnitOfWork _unitOfWork;
+
+		public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+		{
+			_repository = repository;
+			_unitOfWork = unitOfWork;
+		}
+
+		public async Task<T> AddAsync(T entity)
+		{
+			var result = await _repository.AddAsync(entity);
+			await _unitOfWork.CommitAsync();
+			return result;
+		}
+
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _repository.GetAll().ToListAsync();
+		}
+
+		public async Task<T> GetByIdAsync(int id)
+		{
+			var entity = await _repository.GetByIdAsync(id);
+			if (entity == null)
+			{
+				 throw new NotFoundException($"{typeof(T).Name} with id ({id}) not found");
+
+			}
+			return entity;
+		}
+
+		public async Task RemoveAsync(T entity)
+		{
+			_repository.Remove(entity);
+			await _unitOfWork.CommitAsync();
+		}
+
+		public async Task UpdateAsync(T entity)
+		{
+			_repository.Update(entity);
+			await _unitOfWork.CommitAsync();
+		}
+	}
+}
