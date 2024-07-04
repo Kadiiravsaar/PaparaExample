516ca340e4e360cc31a98d5f4b2b50cdecfebc6a
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-05 00:24:22 +0300

    Paged, Sort enum, AppDbContext - Defined SortOrder enum for sorting operations. - Created PagedResultDto<T> and NoContentDto classes for API responses. - Implement AppDbContext with Entity Framework Core

---
+++ RestfulApiExample.Core/Utilities/SortOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Core.Utilities
+{
+	public enum SortOrder
+	{
+		Asc = 1,
+		Desc = 2
+	}
+}
---
+++ RestfulApiExample.Repository/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestfulApiExample.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Repository.Context
+{
+	public class AppDbContext : DbContext
+	{
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
