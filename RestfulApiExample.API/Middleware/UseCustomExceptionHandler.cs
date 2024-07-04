c875d983bf7f6d394ae5d70fb2392899d5fd2d5a
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-05 00:14:21 +0300

    Add Custom Exception Handling - Defined ClientSideException and NotFoundException classes for custom exception handling. - Implemented UseCustomExceptionHandler class to handle exceptions globally.(Middlware)

---
+++ RestfulApiExample.API/Middleware/UseCustomExceptionHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using RestfulApiExample.Core.DTOs;
+using RestfulApiExample.Service.Exceptions;
+using System.Text.Json;
+
+namespace RestfulApiExample.API.Middleware
+{
+	public static class UseCustomExceptionHandler
+	{
+		public static void UseCustomException(this IApplicationBuilder applicationBuilder)
+		{
+
+			applicationBuilder.UseExceptionHandler(config =>
+			{
+
+				config.Run(async context =>
+				{
+					context.Response.ContentType = "application/json";
+
+					var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+					var statusCode = exceptionFeature.Error switch
+					{
+						ClientSideException => 400,
+						NotFoundException => 404,
+						_ => 500
+					};
+					context.Response.StatusCode = statusCode;
+
+					var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+
+					await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
+				});
+			});
+
+		}
+	}
+}
+
---
+++ RestfulApiExample.Service/Exceptions/NotFoundException .cs
@@ -0,0 +1,10 @@
+﻿namespace RestfulApiExample.Service.Exceptions
+{
+	public class NotFoundException : Exception
+	{
+		public NotFoundException(string message) : base(message)
+		{
+
+		}
+	}
+}
