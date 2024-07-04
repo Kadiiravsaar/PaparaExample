69eac4437f9982676abf33d407a79cd5955be6c3
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-05 01:52:17 +0300

    Add Swagger configuration and exception handling middleware
    - Integrated Swagger for API documentation.
    - Configured Swagger to use basic authentication.
    - Added exception handling middleware to log exceptions and handle errors gracefully.

--- RestfulApiExample.API/Program.cs
+++ RestfulApiExample.API/Program.cs
@@ -1,4 +1,17 @@
 
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using RestfulApiExample.API.Extensions;
+using RestfulApiExample.API.Middleware;
+using RestfulApiExample.Core.Repositories;
+using RestfulApiExample.Core.Services;
+using RestfulApiExample.Repository.Context;
+using RestfulApiExample.Repository.Repositories;
+using RestfulApiExample.Service.Mapping;
+using RestfulApiExample.Service.Services;
+using RestfulApiExample.Services.Validations;
+
 namespace RestfulApiExample.API
 {
 	public class Program
@@ -9,10 +22,63 @@ namespace RestfulApiExample.API
 
 			// Add services to the container.
 
-			builder.Services.AddControllers();
+			builder.Services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+			
+
+			// Add custom services
+			builder.Services.AddCustomServices(builder.Configuration);
+
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+			builder.Services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestfulApiExample", Version = "v1" });
+
+				// Add custom headers for authentication
+				c.AddSecurityDefinition("Username", new OpenApiSecurityScheme
+				{
+					Name = "Username",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Description = "Enter your username",
+				});
+
+				c.AddSecurityDefinition("Password", new OpenApiSecurityScheme
+				{
+					Name = "Password",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Description = "Enter your password",
+				});
+
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Username"
+							}
+						},
+						new List<string>()
+					},
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Password"
+							}
+						},
+						new List<string>()
+					}
+				});
+			});
+
+
 
 			var app = builder.Build();
 
@@ -27,6 +93,8 @@ namespace RestfulApiExample.API
 
 			app.UseAuthorization();
 
+			app.UseCustomRequestLog(); // Log Middlware
+			app.UseCustomException(); // Exception Middlware
 
 			app.MapControllers();
 
