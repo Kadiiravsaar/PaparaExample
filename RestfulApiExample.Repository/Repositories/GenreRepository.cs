2e447c9f32df4a2972aa7cbb6e5cbc48e42abf2c
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-16 22:29:19 +0300

    Genre operations continue

---
+++ RestfulApiExample.Repository/Repositories/GenreRepository.cs
@@ -0,0 +1,18 @@
+﻿using RestfulApiExample.Core.Models;
+using RestfulApiExample.Core.Repositories;
+using RestfulApiExample.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Repository.Repositories
+{
+	public class GenreRepository : GenericRepository<Genre>, IGenreRepository
+	{
+		public GenreRepository(AppDbContext context) : base(context)
+		{
+		}
+	}
+}
---
+++ RestfulApiExample.Service/Services/GenreService.cs
@@ -0,0 +1,19 @@
+﻿using RestfulApiExample.Core.Models;
+using RestfulApiExample.Core.Repositories;
+using RestfulApiExample.Core.Services;
+using RestfulApiExample.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Service.Services
+{
+	public class GenreService : Service<Genre>, IGenreService
+	{
+		public GenreService(IGenericRepository<Genre> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+		{
+		}
+	}
+}
