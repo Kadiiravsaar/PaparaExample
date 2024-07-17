ba5ed98860aec96a7dea43af8f0d44bdfdba1b2f
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-18 00:14:17 +0300

    mapping added

--- RestfulApiExample.Service/Mapping/MapProfile.cs
+++ RestfulApiExample.Service/Mapping/MapProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using RestfulApiExample.Core.DTOs;
+using RestfulApiExample.Core.DTOs.Author;
+using RestfulApiExample.Core.DTOs.Book;
+using RestfulApiExample.Core.DTOs.Genre;
 using RestfulApiExample.Core.Models;
 using System;
 using System.Collections.Generic;
@@ -14,7 +17,21 @@ namespace RestfulApiExample.Service.Mapping
 		public MapProfile()
 		{
 			CreateMap<Product, ProductDto>().ReverseMap();
+
 			CreateMap<Product, UpdateProductDto>().ReverseMap();
+
+			CreateMap<Genre, GenreDto>().ReverseMap();
+			CreateMap<Genre, CreateGenreDto>().ReverseMap();
+
+
+			CreateMap<Book, BookDto>().ReverseMap();
+			CreateMap<Book, CreateBookDto>().ReverseMap();
+			CreateMap<Book, UpdateBookDto>().ReverseMap();
+
+			CreateMap<Author, AuthorDto>().ReverseMap();
+			CreateMap<Author, CreateAuthorDto>().ReverseMap();
+			CreateMap<Author, UpdateAuthorDto>().ReverseMap();
 		}
+
 	}
 }
