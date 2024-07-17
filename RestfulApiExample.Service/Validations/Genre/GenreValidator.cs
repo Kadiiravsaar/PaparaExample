8871a32d3beb741395fc9536544ed44c4df6ce54
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-18 00:13:21 +0300

    some classes were deleted

---
+++ RestfulApiExample.Service/Validations/Genre/GenreValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using RestfulApiExample.Core.DTOs.Book;
+using RestfulApiExample.Core.DTOs.Genre;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulApiExample.Service.Validations.Genre
+{
+
+	public class GenreValidator : AbstractValidator<GenreDto>
+	{
+		public GenreValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Tür alanı boş olamaz.");
+		
+			
+		}
+	}
+}
